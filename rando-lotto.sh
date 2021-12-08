a5d84a75d0857616b4580a2fd6b7536fefddec54
K0NXT3D <95604618+K0NXT3D@users.noreply.github.com>
2021-12-07 21:34:35 -0800

    Update rando-lotto.sh

--- rando-lotto.sh
+++ rando-lotto.sh
@@ -3,7 +3,7 @@
 # batch = how many sets of numbers to output (5)
 # count = number of random numbers (5)
 # number = numerical range 1-99
-# output adds a zero to the beginning of single digits 01 02 02 etc..
+# output adds a zero to the beginning of single digits 01 02 03 etc..
 clear
 for batch in {1..5}
  do
